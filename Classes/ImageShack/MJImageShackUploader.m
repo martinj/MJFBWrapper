589ab2e17d73cd7c5010062302fb58a8057b525c
Martin Jonsson <martin.jonsson@gmail.com>
2010-08-17 14:09:32 +0200

    Fixed error handling for imageshackuploader

--- Classes/ImageShack/MJImageShackUploader.m
+++ Classes/ImageShack/MJImageShackUploader.m
@@ -1,32 +1,11 @@
-/*
- *
- * MJImageShackUploader.m
- *  
- * Copyright (c) 2010, Martin Jonsson
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * * Neither the name of the <organization> nor the
- * names of its contributors may be used to endorse or promote products
- * derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+//
+//  MJImageShackUploader.m
+//  SocialTest
+//
+//  Created by Martin Jonsson on 2/4/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
 #import "MJImageShackUploader.h"
 #define CONNECTION_ERROR 1
 #define IMAGESHACK_IMAGE_UPLOAD_URL @"http://www.imageshack.us/upload_api.php"
@@ -49,13 +28,13 @@
 
 
 #pragma mark private methods
-- (NSDictionary *)parseResponseData:(NSData *)responseData {
-	/*
+- (NSDictionary *)parseResponseData:(NSData *)responseData {	
+    /*
 	NSString *str = [[NSString alloc]initWithData:responseData encoding:NSUTF8StringEncoding];
 	NSLog(@"RESPONSE %@", str);
 	[str release];
 	*/
-	
+
 	parsedResponse = [[NSMutableDictionary alloc] init];
 	
 	NSXMLParser *parser = [[NSXMLParser alloc] initWithData:responseData];
@@ -107,11 +86,11 @@
 		}
 	} else {
 		self.connection = nil;
-		NSError *error;
-		NSURLResponse *response;
+		NSError *error = nil;
+		NSURLResponse *response = nil;
 		NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
 
-		if (response == nil || responseData == nil || error != nil) {
+		if (error) {
 			[self didFailWithError:error];
 		} else {
 			return [self parseResponseData:responseData];
